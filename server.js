fd57ee46020cb100e91633400f12ee527e2721d5
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-04-10 19:55:09 -0300

    mongoose config

--- server.js
+++ server.js
@@ -8,17 +8,27 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then((con) => {
-    console.log(con.connections);
-    console.log('DB connected!');
-  });
+mongoose.connect(DB).then(() => {
+  console.log('DB connected!');
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name!'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price!'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = 3000;
 app.listen(port, () => {
