003d96bbc583ac6c81f2f68d7f136a29e6cd064f
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-04-10 22:57:10 -0300

    updated schema for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -48,7 +48,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
@@ -58,6 +58,7 @@ exports.updateTour = async (req, res) => {
     const id = req.params.id;
     const tour = await Tour.findByIdAndUpdate(id, req.body, {
       new: true,
+      runValidators: true,
     });
     res.status(200).json({
       status: 'success',
@@ -73,9 +74,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Tour.findByIdAndDelete(id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.send(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
