e03b53ebddf74eee99ba21028f2c58cff37ddc81
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-04-15 22:30:04 -0300

    added aggregation methods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -81,7 +81,7 @@ exports.updateTour = async (req, res) => {
       },
     });
   } catch (err) {
-    res.send(400).json({
+    res.status(400).json({
       status: 'fail',
       message: err,
     });
@@ -97,7 +97,7 @@ exports.deleteTour = async (req, res) => {
       data: null,
     });
   } catch (err) {
-    res.send(400).json({
+    res.status(400).json({
       status: 'fail',
       message: err,
     });
@@ -106,8 +106,74 @@ exports.deleteTour = async (req, res) => {
 
 exports.getTourStats = async (req, res) => {
   try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
   } catch (err) {
-    res.send(400).json({
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTours: { $sum: 1 },
+        },
+      },
+      {
+        $sort: {
+          numTours: -1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
       status: 'fail',
       message: err,
     });
