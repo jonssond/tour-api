18a33bec10c5868602deb56a2723d39827cc7a08
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-04-17 20:43:24 -0300

    created users model

---
+++ models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const { default: isEmail } = require('validator/lib/isEmail');
+
+//name, email, photo, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please, tell us your name!'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please, provide your e-mail!'],
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid e-mail.'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please insert a password!'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password!'],
+    // validate: {
+    //   validator: function (val) {
+    //     return val === this.password;
+    //   },
+    //   message: 'The passwords must be equal!',
+    // },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './config.env') });
+
 const app = require('./app');
 const mongoose = require('mongoose');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -18,8 +25,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLER REJECTION! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
