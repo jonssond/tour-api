cfa4913680d0bd6ed2fb10b829ec9b4fff786608
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-04-22 21:00:22 -0300

    adding roles

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid e-mail.'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please insert a password!'],
