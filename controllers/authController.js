3b26e127e399c843788193df6f5a59a9c1537da5
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-04-29 22:14:26 -0300

    updated cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,13 +15,20 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
-  res.cookie('jwt', token, {
+  const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    secure: true,
     httpOnly: true,
-  });
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  user.password = undefined;
+
+  res.cookie('jwt', token, cookieOptions);
 
   res.status(statusCode).json({
     status: 'success',
